1575d5d1c86f6588eb0dcb32bce5745820380236
Muaviyaimran <muaviyaimran1122@gmail.com>
2025-05-20 15:37:31 +0500

    Practice State and Props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Header from './Header.svelte';
+	let name = $state('Scott');
+	let status: 'OPEN' | 'CLOSED' = $state('OPEN');
+	function toggle() {
+		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
+	}
+</script>
+
+<Header {name} />
+
+<p>The store is now {status}</p>
+
+<button onclick={toggle}>Toggle Status</button>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { name }: { name: string } = $props();
+</script>
+
+<h1 class="">Hello {name}</h1>
+<h2>{name.replace('t', 'x')}</h2>
+<input type="text" bind:value={name} />
